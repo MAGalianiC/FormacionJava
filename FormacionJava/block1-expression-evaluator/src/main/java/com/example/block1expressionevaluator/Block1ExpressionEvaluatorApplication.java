1b1e308c16f980ac8c7d4b98c43ff1ee2a943cf5
MAGalianiC <miguelangelgaliani@gmail.com>
2022-10-19 10:38:46 +0200

    Proyecto expressionEvaluator creado

---
+++ FormacionJava/block1-expression-evaluator/src/main/java/com/example/block1expressionevaluator/Block1ExpressionEvaluatorApplication.java
@@ -0,0 +1,15 @@
+package com.example.block1expressionevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block1ExpressionEvaluatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block1ExpressionEvaluatorApplication.class, args);
+
+
+	}
+
+}
---
+++ FormacionJava/block1-expression-evaluator/src/test/java/com/example/block1expressionevaluator/Block1ExpressionEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.block1expressionevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block1ExpressionEvaluatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
